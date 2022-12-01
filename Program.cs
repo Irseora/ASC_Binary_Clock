aadd28ffb6d1de67cf19f8797516fc26ceed78a9
Irseora <irseora@gmail.com>
2022-12-01 17:03:43 +0200

    Indicatie

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Scrieti un program C# de consola care afiseaza ora curenta in format binar.
+
+namespace ASC_Binary_Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ASC_Binary_Clock
- 
+
+Scrieti un program C# de consola care afiseaza ora curenta in format binar.  
+
+https://en.wikipedia.org/wiki/Binary_clock
